2979ca71d248d09cc11560f1fa69647a9bc39024
Łukasz Tyszkiewicz <l.tyszkiewicz@qarbon.pl>
2019-09-18 17:53:55 +0200

    Added simple dummy components

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Wrapper = () => {
+  return <div></div>;
+};
+
+export default Wrapper;
---
+++ src/containers/UserFinder.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const UserFinder: FC<{}> = () => {
+  return <Wrapper />;
+};
+
+export default UserFinder;
